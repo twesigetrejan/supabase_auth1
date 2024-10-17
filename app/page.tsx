21757469df2f9c60c41ca51517fa6a06adf12684
twesigetrejan <trejantwesige19@gmail.com>
2024-10-17 08:44:51 +0300

    shark

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default async function Index() {
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <p className="text-4xl">welcome Home</p>
+        <p className="text-4xl">welcome Home, great way to start</p>
       </main>
     </>
   );
