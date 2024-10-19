0f8a4384ccc627aea348d6f80c3cc4e964708486
twesigetrejan <trejantwesige19@gmail.com>
2024-10-19 10:21:07 +0300

    default

--- components/AddForm.tsx
+++ components/AddForm.tsx
@@ -23,7 +23,7 @@ export function AddForm() {
         <div className="">
             <form action={CreateTodo}>
                 <label htmlFor="todo" className="">Enter Task</label>
-                <input type="text" name="todo" className="" required />
+                <input type="text" name="todo" className="px-1" required />
                 <SubmitButton />
             </form>
 
